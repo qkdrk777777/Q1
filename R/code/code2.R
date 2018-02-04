add9017264b7aa4259fd17bdf62fbdc559275782
qkdrk777777 <qkdrk777777@naver.com>
2018-02-04 23:22:29 +0900

    update

--- R/code/code2.R
+++ R/code/code2.R
@@ -5,9 +5,11 @@ e.data<-data2[data2$학교급==" 초",]
 colnames(e.data)
 
 #시군구 출력
+
 t<-addreturn(e.data,addvar=5,matrix=T)
 colnames(t)<-c('시','군','구')
 package(stringr)
+
 rowdata2<-cbind(e.data,t)
 
 #devtools::use_data(rowdata2, internal = F)
