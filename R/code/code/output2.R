fd715a266d63bbb7d1d56609e21dfd10da65224a
qkdrk777777 <qkdrk777777@naver.com>
2018-02-09 00:44:24 +0900

    update

--- R/code/code/output2.R
+++ R/code/code/output2.R
@@ -100,6 +100,7 @@ area<-rbind(area,x)
 area[,2]<-as.integer(area[,2])
 
 t<-merge(tdata,area,by='V11',all=T)
+
 names(t)
 sum(is.na(t[,6])==T)
 sum(is.na(t[,7])==T)
@@ -130,6 +131,7 @@ out7<-ddply(t,~V11,summarise,학교당평균면적=sum(X2016)/sum(count))
 output<-merge(merge(merge(merge(merge(merge(merge(out0,out1,by='V11',all=T),out2,by='V11',all=T),out3,by='V11',all=T),out4,by='V11',all=T),out5,by='V11',all=T),out6,by='V11',all=T),out7,by='V11',all=T)
 
 summary(output)
+
 i=3
 #par(mfrow=c(2,3))
 #par(mfrow=c(1,1))
@@ -150,7 +152,7 @@ tttt
 if(legend){p=locator(1)
 legend(p,legend=tttt,col=colors,pch=16,cex=0.8,bty='n')}
 }
-par(mfrow=c(1,2))
+par(mfrow=c(1,3))
 #pp(2)
 #############지니 엣킨슨
 #sum(t[t$V11%in%'강남구',12])
@@ -158,21 +160,21 @@ par(mfrow=c(1,2))
 #dim(t)
 #시군구별 학교당 임원수의 불평등지수
 
-out0<-ddply(t,~V11,summarise,학교당임원수_지니=ineq((TOT_OFWR_CNT)/(count),type='Atkinson'))
+out0<-ddply(t,~V11,summarise,학교당임원수_지니=ineq((TOT_OFWR_CNT)/(count)))#,type='Atkinson'))
 #시군구별 평균 학급당 학생수의 불평등지수
-out1<-ddply(t,~V11,summarise,학급당학생수_지니=ineq((TOT_STDT_CNT)/(TOT_CLASS_CNT),type='Atkinson'))
+out1<-ddply(t,~V11,summarise,학급당학생수_지니=ineq((TOT_STDT_CNT)/(TOT_CLASS_CNT)))#,type='Atkinson'))
 #시군구별 평균 학교당 교원수의 불평등지수
-out2<-ddply(t,~V11,summarise,학교당교원수_지니=ineq((TOT_TCHR_CNT)/(count),type='Atkinson'))
+out2<-ddply(t,~V11,summarise,학교당교원수_지니=ineq((TOT_TCHR_CNT)/(count)))#,type='Atkinson'))
 #시군구별 학교당 학급수의 불평등지수
-out3<-ddply(t,~V11,summarise,학교당학급수_지니=ineq((TOT_CLASS_CNT)/(count),type='Atkinson'))
+out3<-ddply(t,~V11,summarise,학교당학급수_지니=ineq((TOT_CLASS_CNT)/(count)))#,type='Atkinson'))
 #시군구별 교원당 학생수의 불평등지수
-out4<-ddply(t,~V11,summarise,교원당학생수_지니=ineq((TOT_STDT_CNT)/(TOT_TCHR_CNT),type='Atkinson'))
+out4<-ddply(t,~V11,summarise,교원당학생수_지니=ineq((TOT_STDT_CNT)/(TOT_TCHR_CNT)))#,type='Atkinson'))
 #시군구별 면적당 학생수의 불평등지수
-out5<-ddply(t,~V11,summarise,km제곱당학생수=ineq((TOT_STDT_CNT)/(X2016),type='Atkinson'))
+out5<-ddply(t,~V11,summarise,km제곱당학생수_지니=ineq((TOT_STDT_CNT)/(X2016)))#,type='Atkinson'))
 #시군구별 학급당 임원수의 불평등지수
-out6<-ddply(t,~V11,summarise,학급당임원수_지니=ineq((TOT_OFWR_CNT)/(TOT_CLASS_CNT),type='Atkinson'))
+out6<-ddply(t,~V11,summarise,학급당임원수_지니=ineq((TOT_OFWR_CNT)/(TOT_CLASS_CNT)))#,type='Atkinson'))
 #시군구별 학교당 평균 면적
-out7<-ddply(t,~V11,summarise,학교당평균면적_지니=mean(X2016/count,type='Atkinson'))
+out7<-ddply(t,~V11,summarise,학교당평균면적=sum(X2016)/sum(count))
 
 output2<-merge(merge(merge(merge(merge(merge(merge(out0,out1,by='V11',all=T),out2,by='V11',all=T),out3,by='V11',all=T),out4,by='V11',all=T),out5,by='V11',all=T),out6,by='V11',all=T),out7,by='V11',all=T)
 summary(output2)
@@ -191,21 +193,78 @@ pp2<-function(i,legend=F){
   legend(p,legend=tttt,col=colors,pch=16,cex=0.8,bty='n')}
 }
 summary(output2)
+
+
+############
+#엣킨슨
+
+out0<-ddply(t,~V11,summarise,학교당임원수_앳킨슨=ineq((TOT_OFWR_CNT)/(count),type='Atkinson'))
+#시군구별 평균 학급당 학생수의 불평등지수
+out1<-ddply(t,~V11,summarise,학급당학생수_앳킨슨=ineq((TOT_STDT_CNT)/(TOT_CLASS_CNT),type='Atkinson'))
+#시군구별 평균 학교당 교원수의 불평등지수
+out2<-ddply(t,~V11,summarise,학교당교원수_앳킨슨=ineq((TOT_TCHR_CNT)/(count),type='Atkinson'))
+#시군구별 학교당 학급수의 불평등지수
+out3<-ddply(t,~V11,summarise,학교당학급수_앳킨슨=ineq((TOT_CLASS_CNT)/(count),type='Atkinson'))
+#시군구별 교원당 학생수의 불평등지수
+out4<-ddply(t,~V11,summarise,교원당학생수_앳킨슨=ineq((TOT_STDT_CNT)/(TOT_TCHR_CNT),type='Atkinson'))
+#시군구별 면적당 학생수의 불평등지수
+out5<-ddply(t,~V11,summarise,km제곱당학생수_앳킨슨=ineq((TOT_STDT_CNT)/(X2016),type='Atkinson'))
+#시군구별 학급당 임원수의 불평등지수
+out6<-ddply(t,~V11,summarise,학급당임원수_앳킨슨=ineq((TOT_OFWR_CNT)/(TOT_CLASS_CNT),type='Atkinson'))
+#시군구별 학교당 평균 면적
+out7<-ddply(t,~V11,summarise,학교당평균면적=sum(X2016)/sum(count))
+
+output3<-merge(merge(merge(merge(merge(merge(merge(out0,out1,by='V11',all=T),out2,by='V11',all=T),out3,by='V11',all=T),out4,by='V11',all=T),out5,by='V11',all=T),out6,by='V11',all=T),out7,by='V11',all=T)
+summary(output3)
+pp3<-function(i,legend=F){
+  colors =(brewer.pal(9,"YlOrRd"))
+  qqq<-match(q[[1]]@data$SIG_KOR_NM,output3$V11)
+  col<-as.numeric(cut(output3[,i],seq(floor(min(output3[,i],na.rm=T)),ceiling(max(output3[,i],na.rm=T)),length=10)))
+  plot(q[[1]],col=colors[col[qqq]],main=colnames(output3)[i],cex.main=2.5)
+  tt<-seq(floor(min(output3[,i],na.rm=T)),ceiling(max(output3[,i],na.rm=T)),length=9)
+  tttt<-NULL
+  for(i in 2:length(tt)-1)
+  {ttt<-paste0(tt[i],'~',tt[i+1])
+  tttt<-c(tttt,ttt)}
+  tttt
+  if(legend){p=locator(1)
+  legend(p,legend=tttt,col=colors,pch=16,cex=0.8,bty='n')}
+}
+summary(output3)
+
+##plot
 pp(2,legend=T)
 pp2(2,legend=T)
+pp3(2,legend=T)
 pp(3,legend=T)
 pp2(3,legend=T)
+pp3(3,legend=T)
 pp(4,legend=T)
 pp2(4,legend=T)
+pp3(4,legend=T)
 pp(5,legend=T)
 pp2(5,legend=T)
+pp3(5,legend=T)
 pp(6,legend=T)
 pp2(6,legend=T)
+pp3(6,legend=T)
 pp(7,legend=T)
 pp2(7,legend=T)
+pp3(7,legend=T)
 pp(8,legend=T)
 pp2(8,legend=T)
+pp3(8,legend=T)
 par(mfrow=c(1,1))
 pp(9,legend=T)
 
-legend(locator(1),legend=paste0('전국의 불평등지수\n\n',round(ineq(out7[,2]),5)),bty='n')
+legend(locator(1),legend=paste0('불평등지수\n\n',round(ineq(out7[,2]),5),'\n',round(ineq(out7[,2],type='Atkinson'),5)),bty='n')
+fix(rowdata)
+head(rowdata2,3)
+dim(rowdata2)
+colnames(output)
+i=2
+par(mfrow=c(2,4))
+for(i in 2:9)
+plot(Lc(output[,i]),col='darkred',main=paste(colnames(output)[i],'로렌츠곡선'))
+output
+output2
